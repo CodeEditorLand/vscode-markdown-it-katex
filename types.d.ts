bf860e34ec04514084339218df4adb53457698c5
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 14:18:55 +0300

    squash!

--- types.d.ts
+++ types.d.ts
@@ -1,25 +1,28 @@
-import type MarkdownIt from 'markdown-it';
+import type MarkdownIt from "markdown-it";
 
 export interface MarkdownKatexOptions {
-    /**
-     * Enable rendering of bare `\begin` `\end` blocks without `$$` delimiters.
-     */
-    readonly enableBareBlocks?: boolean;
+	/**
+	 * Enable rendering of bare `\begin` `\end` blocks without `$$` delimiters.
+	 */
+	readonly enableBareBlocks?: boolean;
 
-    /**
-     * Enable rendering of `$$` match blocks inside of html elements.
-     */
-    readonly enableMathBlockInHtml?: boolean;
+	/**
+	 * Enable rendering of `$$` match blocks inside of html elements.
+	 */
+	readonly enableMathBlockInHtml?: boolean;
 
-    /**
-     * Enable rendering of inline match of html elements.
-     */
-    readonly enableMathInlineInHtml?: boolean;
+	/**
+	 * Enable rendering of inline match of html elements.
+	 */
+	readonly enableMathInlineInHtml?: boolean;
 
-    /**
-     * Controls if an exception is thrown on katex errors.
-     */
-    readonly throwOnError?: boolean;
+	/**
+	 * Controls if an exception is thrown on katex errors.
+	 */
+	readonly throwOnError?: boolean;
 }
 
-export default function (md: MarkdownIt, options?: MarkdownKatexOptions): MarkdownIt;
+export default function (
+	md: MarkdownIt,
+	options?: MarkdownKatexOptions,
+): MarkdownIt;
